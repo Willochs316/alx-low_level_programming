97aebe3cfc55b318879f757d9539551afc806d7d
Willochs Ojigbo <82782869+Willochs316@users.noreply.github.com>
2022-06-13 16:37:22 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,29 +7,29 @@
  */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
 
-while (*(s + count) != '\0')
-{
-if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-break;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
-if (*(s + count) == '-')
-k *= -1;
+		if (*(s + count) == '-')
+			k *= -1;
 
-if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-{
-if (size > 0)
-m *= 10;
-size++;
-}
-count++;
-}
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
 
-for (i = count - size; i < count; i++)
-{
-j = j + ((*(s + i) - 48) * m);
-m /= 10;
-}
-return (j * k);
+	for (i = count - size; i < count; i++)
+	{
+		j = j + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (j * k);
 }
